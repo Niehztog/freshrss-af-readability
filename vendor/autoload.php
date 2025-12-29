b354bff920fe7f0180b7f9c5e2772edeea32b90e
Rob Loach <robloach@gmail.com>
2025-12-29 18:04:57 -0500

    Update Dependencies with Composer optimization

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -14,12 +14,9 @@ if (PHP_VERSION_ID < 50600) {
             echo $err;
         }
     }
-    trigger_error(
-        $err,
-        E_USER_ERROR
-    );
+    throw new RuntimeException($err);
 }
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitb44cc79a0eaef9cd9c2f2ac697cbe9c0::getLoader();
+return ComposerAutoloaderInit99647747e3c776c5348978255603b24a::getLoader();
